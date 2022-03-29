a46110490f72b13900b6a0ef065a292ef6a80a60
Yilika Loufoua <yilika.loufoua@tcu.edu>
2022-03-29 13:44:40 -0500

    renamed files for clearer structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import { createWebHistory, createRouter } from "vue-router"
 import Home from "../components/Home.vue"
-import SuperFrogTeam from "../components/UserAccountManagement/SuperFrogTeam/SuperFrogTeam.vue";
+import SuperFrogTeam from "../components/UserAccountManagement/SuperFrogTeam/SuperFrogTeamRequestsView.vue";
 import Customer from "../components/UserAccountManagement/Customer/CustomerRequestsView.vue";
-import SpiritDirector from "../components/UserAccountManagement/SpiritDirector/SpiritDirector.vue";
+import SpiritDirector from "../components/UserAccountManagement/SpiritDirector/SpiritDirectorRequestsView.vue";
 
 const routes = [
   {
