64ead6e3418149f19779624ed8071f6c0bdc0de5
Chase Lennartson <chaselennartson@Chases-MacBook-Air-2.local>
2022-04-19 19:26:16 -0500

    profile page

---
+++ src/components/Tables/SuperFrogTeamProfile.vue
@@ -0,0 +1,47 @@
+<script setup>
+    let profileList = [
+        {
+          name: 'Bob Smith',
+          email: 'b.smith@tcu.edu',
+          dateOfBirth: '10/23/1987',
+          phone: '817-528-1249',
+          address: '2800 S University Dr',
+        },
+    ]
+</script>
+
+<template>
+  <div class="container col py-3" id="app">
+    <div class="card">
+      <div class="card-body d-flex justify-content-between">
+        <div><!-- empty div so content-between looks good --></div>
+        <h4 class="card-title">Profile</h4>
+      </div>
+    </div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Customer Name</th>
+          <th scope="col">E-Mail</th>
+          <th scope="col">Date of Birth</th>
+          <th scope="col">Phone Number</th>
+          <th scope="col">Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="profile in profileList" :key="profile.id">
+          <td>{{ profile.name }}</td>
+          <td>{{ profile.email }}</td>
+          <td>{{ profile.dateOfBirth }}</td>
+          <td>{{ profile.phone }}</td>
+          <td>{{ profile.address }}</td>
+          <td>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+</style>
--- src/components/UserAccountManagement/CustomerDashboard.vue
+++ src/components/UserAccountManagement/CustomerDashboard.vue
@@ -1,16 +1,36 @@
 <script setup>
 import CustomerSideBar from "../Sidebars/CustomerSideBar.vue"
 import CustomerTable from "../Tables/CustomerTable.vue"
+import CustomerProfile from "../Tables/CustomerProfile.vue"
+import { useRouter } from 'vue-router';
 
-defineProps({
-  msg: String
-})
+export default {
+  components: {useRouter, CustomerSideBar, CustomerTable, CustomerProfile },
+  data() {
+    return {
+      currentPath: useRouter().currentRoute.value
+    }
+  },
+  computed: {
+    currentView() {
+      return this.currentPath.name
+    }
+  },
+  mounted() {
+    window.addEventListener('hashchange', () => {
+      this.currentPath = window.location.hash
+    })
+  }
+}
 </script>
 
 <template>
-  <div class="dashboard">
-    <CustomerSideBar/>
-    <CustomerTable/>
+  <div class="container-fluid">
+    <div class = "row flex-nowrap">
+      <CustomerSideBar/>
+      <CustomerTable/>
+     <component :is="currentView" />
+    </div>
   </div>
 </template>
 
