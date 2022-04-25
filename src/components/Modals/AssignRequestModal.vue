2f8751a434077cf3da8427da81280d347e9c1de6
Yilika Loufoua <yilika.loufoua@tcu.edu>
2022-04-25 03:01:53 -0500

    Add action buttons

---
+++ src/components/Modals/AssignRequestModal.vue
@@ -0,0 +1,103 @@
+<script setup>
+  // TODO: GET student list
+  let studentList = [
+      {
+        id: 0,
+        name: 'Yilika Loufoua',
+        email: 'y.loufoua@tcu.edu',
+        phone: '817-990-3244',
+        classification: 'Senior',
+        requestsTaken: 3,
+      },
+      {
+        id: 1,
+        name: 'Quynh Dong',
+        email: 'q.dong@tcu.edu',
+        phone: '817-234-9843',
+        classification: 'Sophomore',
+        requestsTaken: 1,
+      },
+      {
+        id: 2,
+        name: 'Chase Lennartson',
+        email: 'c.lennartson@tcu.edu',
+        phone: '817-039-2935',
+        classification: 'Junior',
+        requestsTaken: 2,
+      },
+      {
+        id: 3,
+        name: 'Lucas Karwal',
+        email: 'l.karwal@tcu.edu',
+        phone: '682-323-5236',
+        classification: 'Senior',
+        requestsTaken: 5,
+      }
+  ]
+</script>
+
+<template>
+  <div
+    class="modal fade"
+    :id="modalID"
+    data-bs-backdrop="static"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-lg">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="staticBackdropLabel">Assign Super Frog</h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="modal-body">
+          <table class="table">
+              <thead>
+                  <tr>
+                      <th scope="col">Name</th>
+                      <th scope="col">Classification</th>
+                      <th scope="col">Requests Taken</th>
+                  </tr>
+              </thead>
+              <tbody>
+                <tr v-for="student in studentList" :key="student.id">
+                  <td>{{student.name}}</td>
+                  <td>{{student.classification}}</td>
+                  <td>{{student.requestsTaken}}</td>
+                  <td>
+                    <button type="button" class="btn btn-success btn-sm" data-bs-dismiss="modal">Select</button>
+                  </td>
+                </tr>
+              </tbody>
+          </table>
+        </div>
+        <div class="modal-footer">
+          <button
+            type="button"
+            class="btn btn-secondary"
+            data-bs-dismiss="modal"
+            @click="clearInputFields">Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.datepicker {
+  width: 25%;
+}
+
+.table-condensed {
+  width: 100%;
+  text-align: center;
+}
+</style>
--- src/components/Tables/SpiritDirectorTeamTable.vue
+++ src/components/Tables/SpiritDirectorTeamTable.vue
@@ -1,36 +1,41 @@
 <script setup>
+import EditStudentModal from "../Modals/EditStudentModal.vue";
     let studentList = [
         {
-          id: 0,
+          id: 1,
           name: 'Yilika Loufoua',
           email: 'y.loufoua@tcu.edu',
           phone: '817-990-3244',
           classification: 'Senior',
           requestsTaken: 3,
+          isActive: true
         },
         {
-          id: 1,
+          id: 2,
           name: 'Quynh Dong',
           email: 'q.dong@tcu.edu',
           phone: '817-234-9843',
           classification: 'Sophomore',
           requestsTaken: 1,
+          isActive: true
         },
         {
-          id: 2,
+          id: 3,
           name: 'Chase Lennartson',
           email: 'c.lennartson@tcu.edu',
           phone: '817-039-2935',
           classification: 'Junior',
           requestsTaken: 2,
+          isActive: true
         },
         {
-          id: 3,
+          id: 4,
           name: 'Lucas Karwal',
           email: 'l.karwal@tcu.edu',
           phone: '682-323-5236',
           classification: 'Senior',
           requestsTaken: 5,
+          isActive: false
         }
     ]
 </script>
@@ -42,6 +47,7 @@
                 <h4 class="card-title">SuperFrog Team</h4>
             </div>
         </div>
+        <EditStudentModal id="edit-student-modal" />
         <table class="table">
             <thead>
                 <tr>
@@ -51,6 +57,8 @@
                     <th scope="col">Phone</th>
                     <th scope="col">Classification</th>
                     <th scope="col">Requests Taken</th>
+                    <th scope="col">Status</th>
+                    <th scope="col" colspan="2">Actions</th> 
                 </tr>
             </thead>
             <tbody>
@@ -61,6 +69,21 @@
                 <td>{{student.phone}}</td>
                 <td>{{student.classification}}</td>
                 <td>{{student.requestsTaken}}</td>
+                <td>
+                  <span class="badge bg-success" v-if="student.isActive">Active</span>
+                  <span class="badge bg-danger" v-else>Inactive</span>
+                </td>
+                <td>
+                  <button 
+                    type="button" 
+                    class="btn btn-light btn-sm btn-outline-dark" 
+                    data-bs-toggle="modal" 
+                    data-bs-target="#edit-student-modal">Edit</button>
+                </td>
+                <td>
+                  <button v-if="student.isActive" type="button" class="btn btn-danger btn-sm me-2">Deactivate</button>
+                  <button v-else type="button" class="btn btn-success btn-sm">Reactivate</button>
+                </td>
               </tr>
             </tbody>
         </table>
