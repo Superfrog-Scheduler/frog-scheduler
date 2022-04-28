0f0797812e6a58c48d5324dd5c365a30afa9c591
Yilika Loufoua <yilika.loufoua@tcu.edu>
2022-04-28 00:23:20 -0500

    Customer make new requests

---
+++ src/store/requestStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from 'pinia';
+import requestApi from '@/apis/requestApi';
+
+export default defineStore('request', {
+  state: () => {
+    return {
+      requestInfo: {},
+      id: 0,
+    };
+  },
+  actions: {
+    async getrequestInfo(token) {
+      const res = await requestApi.info(token, this.id);
+      
+      this.requestInfo = res.data;
+      console.log("request", this.requestInfo)
+    },
+  },
+});
